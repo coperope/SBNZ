0f64fb06f67f4df8b5e3589988a5e61b87076bf6
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2020-05-16 19:23:12 +0200

    A-3.1: Initial rules and testing template

---
+++ CarRR-app/src/test/java/rules_tests/TaggingTests.java
@@ -0,0 +1,50 @@
+package rules_tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import main.facts.Tag;
+import main.facts.Vehicle;
+
+public class TaggingTests {
+	
+    protected final String ksessionName = "categorisation_tagging_session";
+    private KieServices ks;
+    private KieContainer kieContainer;
+    private KieSession kieSession;
+    
+    @Before
+    public void init() {
+    	ks = KieServices.Factory.get();
+    	kieContainer = ks.getKieClasspathContainer();
+    	kieSession = kieContainer.newKieSession("categorisation_tagging_session");
+    }
+    
+    @Test
+    public void testHighFuelConsumptionTag() {
+    	Tag tag = new Tag();
+    	tag.setName("High fuel consumption");
+    	Vehicle vehicle = new Vehicle();
+    	vehicle.setFuelConsumption(13l);
+    	vehicle.setTags(new ArrayList<Tag>());
+    	
+    	kieSession.insert(tag);
+    	kieSession.insert(vehicle);
+		kieSession.fireAllRules();
+		kieSession.dispose();
+		
+		System.out.println(vehicle);
+		assertEquals(1, vehicle.getTags().size());
+    }
+    
+
+    
+
+}
