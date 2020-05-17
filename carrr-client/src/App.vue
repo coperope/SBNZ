116754b5976b2bb4b8b87ddceb92d4bc0e8fed9e
DamjanPantic <panticdamjan@gmail.com>
2020-05-17 13:37:50 +0200

    F-1: Initial client project

---
+++ carrr-client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ carrr-client/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-container class="mt-4">
+    <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
+      <template v-slot:header>Developers</template>
+
+      <hr class="my-4" />
+      <ul>
+        <li>Petar Basić</li>
+        <li>Mihajlo Perendija</li>
+        <li>Damjan Pantić</li>
+      </ul>
+    </b-jumbotron>
+  </b-container>
+</template>
