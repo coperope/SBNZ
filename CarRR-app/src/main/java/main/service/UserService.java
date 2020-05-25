7f64f06c24e30fda271a4d97741bad954748d844
unknown <petrosbasic@gmail.com>
2020-05-25 17:50:43 +0200

    B-1: Inital user login/register controller.

--- CarRR-app/src/main/java/main/service/UserService.java
+++ CarRR-app/src/main/java/main/service/UserService.java
@@ -1,5 +1,7 @@
 package main.service;
 
+import main.dto.UserDTO;
+import main.facts.User;
 import main.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,20 @@ public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+
+    public User findByEmail(String email)  {
+        User u = userRepo.findByEmail(email);
+        return u;
+    }
+
+    public UserDTO registerUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setSurname(userDTO.getSurname());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+        userRepo.save(user);
+        return userDTO;
+    }
 }
