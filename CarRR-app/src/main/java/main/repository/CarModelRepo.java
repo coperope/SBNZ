3dedd19697683ea62fb87b3baff2abd0dac3d52c
unknown <petrosbasic@gmail.com>
2020-05-26 22:57:35 +0200

    B-2: Initial vehicle controller and service.

--- CarRR-app/src/main/java/main/repository/CarModelRepo.java
+++ CarRR-app/src/main/java/main/repository/CarModelRepo.java
@@ -1,7 +1,11 @@
 package main.repository;
 
+import main.facts.Brand;
 import main.facts.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarModelRepo extends JpaRepository<CarModel, Long> {
+    List<CarModel> getAllByBrand(Brand brand);
 }
