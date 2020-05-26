6653348e6cb2fdae847d915c72cf18b428d7a165
unknown <petrosbasic@gmail.com>
2020-05-26 21:41:47 +0200

    B-1: Vehicle parts service

--- CarRR-app/src/main/java/main/repository/CarModelRepo.java
+++ CarRR-app/src/main/java/main/repository/CarModelRepo.java
@@ -1,7 +1,12 @@
 package main.repository;
 
+import main.facts.Brand;
 import main.facts.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+
 public interface CarModelRepo extends JpaRepository<CarModel, Long> {
+
+    ArrayList<CarModel> getAllByBrand(Brand brand);
 }
