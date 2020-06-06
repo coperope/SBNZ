6cac67e7f5b864ee25ff3938f825360f9423adbe
unknown <petrosbasic@gmail.com>
2020-06-06 21:48:18 +0200

    B-7-8: Fix rules new vehicle event.

--- CarRR-app/src/main/java/main/facts/Bundle.java
+++ CarRR-app/src/main/java/main/facts/Bundle.java
@@ -2,8 +2,12 @@ package main.facts;
 
 public class Bundle {
     private int finishedRules;
-
+    private Long vehicleID;
     public Bundle() {
+    }
+
+    public Bundle(Long vehicleID) {
+        this.vehicleID = vehicleID;
     }
 
     public int getFinishedRules() {
@@ -16,5 +20,13 @@ public class Bundle {
 
     public void increaseFinishedRules(){
         this.finishedRules = this.finishedRules +1;
+    }
+
+    public Long getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(Long vehicleID) {
+        this.vehicleID = vehicleID;
     }
 }
